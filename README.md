d1d4d909cffa2eb5a1b3c73d09767f620ab7a4d7
Amol Ghatol <amol_g@techjoomla.com>
2021-05-24 18:01:59 +0530

    Run instructions and single collection run support

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# postman-testcase
-Postman testcases runner script
+### Run multiple collections sharing a common environment
+
+There is no feature available in postman to do this, But there is a workaround I discovered
+
+Add following code at line 98 (after "var emitter = new EventEmitter(),runner = new runtime.Runner(),stopOnFailure,entrypoint;") (inside node_modules/newman/lib/run/index.js):
+```
+if(_.isArray(options.collection)){
+        let obj = {};
+        obj.info = options.collection[0].info;
+        obj.info.name = "combined";
+        obj.item = [];
+        for(let i=0; i<options.collection.length; i++){
+            obj.item.push({name:"file"+(i+1),item:options.collection[i].item});
+        }
+        options.collection = obj;
+}
+```
+refer: https://github.com/postmanlabs/newman/issues/2262
+
+## Run script
+Navigate to script folder and run commands
+```
+$ npm install
+$ node collection-runner.js
+```
+To run specific collection use below command 
+```
+$ node collection-runner.js -f collection_file_path 
+```
--- script/collection-runner.js
+++ script/collection-runner.js
@@ -1,8 +1,10 @@
 const newman = require('newman');
+const yargs = require('yargs');
 const path = require('path');
 const fs = require('fs');
 const fsPromises = fs.promises;
 const collections = [];
+const argv = yargs.argv;
 
 async function listDir(dirPath) {
     try {
@@ -13,38 +15,49 @@ async function listDir(dirPath) {
 }
 
 (function(){
-    const collectionsDir = path.join(__dirname, '../collections');
-    listDir(collectionsDir).then((teams, err) => {
-        if (err)
-            throw err;
-        try {
-            teams.forEach(teamFolder => {
-                let teamCollections = listDir(path.join(__dirname, '../collections/' + teamFolder));
-                collections.push(teamCollections);
-            });
+    if (argv.f) {
+        newman.run({
+            collection: [require(argv.f)],
+            reporters: ['htmlextra', 'cli'],
+            environment: require('./env.json')
+        }, function (er) {
+            if (er) { throw er; }
+            console.log('collection run complete!');
+        })
+    } else {
+        const collectionsDir = path.join(__dirname, '../collections');
+        listDir(collectionsDir).then((teams, err) => {
+            if (err)
+                throw err;
+            try {
+                teams.forEach(teamFolder => {
+                    let teamCollections = listDir(path.join(__dirname, '../collections/' + teamFolder));
+                    collections.push(teamCollections);
+                });
 
-            Promise.all(collections).then(files => {
-                const filesWithPath = files.map((file, index)=> file.map(val => path.join(__dirname, '../collections/' + teams[index]) + '/' + val));
-                console.log('############## COLLECTIONS TO RUN #################');
-                console.log(filesWithPath);
-                console.log('###################################################');
-                const collectionsToRequire = filesWithPath.flat().map(val => require(val));
+                Promise.all(collections).then(files => {
+                    const filesWithPath = files.map((file, index)=> file.map(val => path.join(__dirname, '../collections/' + teams[index]) + '/' + val));
+                    console.log('############## COLLECTIONS TO RUN #################');
+                    console.log(filesWithPath);
+                    console.log('###################################################');
+                    const collectionsToRequire = filesWithPath.flat().map(val => require(val));
 
-                newman.run({
-                    collection: collectionsToRequire,
-                    reporters: ['htmlextra', 'cli'],
-                    environment: require('./env.json')
-                }, function (er) {
-                    if (er) { throw er; }
-                    console.log('collection run complete!');
+                    newman.run({
+                        collection: collectionsToRequire,
+                        reporters: ['htmlextra', 'cli'],
+                        environment: require('./env.json')
+                    }, function (er) {
+                        if (er) { throw er; }
+                        console.log('collection run complete!');
+                    })
+
+                },(error) => {
+                    console.error("Error reading collections", error);
                 })
 
-            },(error) => {
-                console.error("Error reading collections", error);
-            })
-
-        } catch (error) {
-            console.error('Error occured while reading directory!', error);
-        }
-    });
+            } catch (error) {
+                console.error('Error occured while reading directory!', error);
+            }
+        });
+    }
 })();
