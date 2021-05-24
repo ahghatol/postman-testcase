dc86ef7ccd0d42455fdc2316d51fc9c83a59f746
Amol Ghatol <amol_g@techjoomla.com>
2021-05-24 09:46:53 +0530

    Postman collection testcases runner script

---
+++ script/collection-runner.js
@@ -0,0 +1,50 @@
+const newman = require('newman');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const collections = [];
+
+async function listDir(dirPath) {
+    try {
+        return fsPromises.readdir(dirPath);
+    } catch (err) {
+        console.error('Error occured while reading directory!', err);
+    }
+}
+
+(function(){
+    const collectionsDir = path.join(__dirname, '../collections');
+    listDir(collectionsDir).then((teams, err) => {
+        if (err)
+            throw err;
+        try {
+            teams.forEach(teamFolder => {
+                let teamCollections = listDir(path.join(__dirname, '../collections/' + teamFolder));
+                collections.push(teamCollections);
+            });
+
+            Promise.all(collections).then(files => {
+                const filesWithPath = files.map((file, index)=> file.map(val => path.join(__dirname, '../collections/' + teams[index]) + '/' + val));
+                console.log('############## COLLECTIONS TO RUN #################');
+                console.log(filesWithPath);
+                console.log('###################################################');
+                const collectionsToRequire = filesWithPath.flat().map(val => require(val));
+
+                newman.run({
+                    collection: collectionsToRequire,
+                    reporters: ['htmlextra', 'cli'],
+                    environment: require('./env.json')
+                }, function (er) {
+                    if (er) { throw er; }
+                    console.log('collection run complete!');
+                })
+
+            },(error) => {
+                console.error("Error reading collections", error);
+            })
+
+        } catch (error) {
+            console.error('Error occured while reading directory!', error);
+        }
+    });
+})();
